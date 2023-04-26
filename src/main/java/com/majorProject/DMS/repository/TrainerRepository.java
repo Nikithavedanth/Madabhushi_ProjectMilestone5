3757c84386446aba35c0e8397497487846166bc6
Nikithavedanth <nikithavedanth@gmail.com>
2023-04-26 15:34:41 -0500

    Project Milestone5

---
+++ src/main/java/com/majorProject/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.majorProject.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.majorProject.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
